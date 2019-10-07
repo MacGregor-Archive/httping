9f30898ef92e49cd2f99ea9b590c75aca1f8f840
Richard Taylor <moomerman@gmail.com>
2019-10-07 20:25:21 +0100

    Get configuration from .env

--- config/config.exs
+++ config/config.exs
@@ -9,12 +9,12 @@ use Mix.Config
 
 # Configures the endpoint
 config :live_view_demo, LiveViewDemoWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "dummy_val_for_dev_env",
+  url: [host: System.get_env("HOST") || "localhost"],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   render_errors: [view: LiveViewDemoWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: LiveViewDemo.PubSub, adapter: Phoenix.PubSub.PG2],
   live_view: [
-    signing_salt: "another_dummy_val_for_dev_env"
+    signing_salt: System.get_env("SIGNING_SALT")
   ]
 
 # Configures Elixir's Logger
